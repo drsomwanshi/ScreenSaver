1497b52931e371a2990a55674c7971c4c24c5e23
drsomwanshi <somwanshi1234@gmail.com>
2023-07-07 10:29:53 +0530

    Add project files.

---
+++ ScreenSaver/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSaver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int n = 1;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile
+                ("d:\\img\\"+n+".jpg");
+            n = n + 1;
+            if (n > 5)
+                n = 1;
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {                
+                Application.Exit();
+            }
+        }
+    }
+}
